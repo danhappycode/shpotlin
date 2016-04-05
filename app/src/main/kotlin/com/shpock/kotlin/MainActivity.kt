ee95823ed20a550ce3d025359bd6650d4bc555db
Daniel Niedermühlbichler <dn@shpock.com>
2016-04-05 20:26:19 +0200

    implement reload count tracking

--- app/src/main/kotlin/com/shpock/kotlin/MainActivity.kt
+++ app/src/main/kotlin/com/shpock/kotlin/MainActivity.kt
@@ -3,25 +3,44 @@ package com.shpock.kotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ItemService.ResponseListener {
+
+    private lateinit var tracking: Tracking
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = ItemAdapter(this)
+        tracking = Tracking(getPreferences(MODE_PRIVATE))
 
-        swipeRefreshLayout.setOnRefreshListener { loadItems() }
+        setUpRecyclerView()
+        setUpSwipeRefresh()
 
         loadItems()
+    }
+
+    private fun setUpSwipeRefresh() {
+        swipeRefreshLayout.setOnRefreshListener { loadItems() }
+    }
+
+    private fun setUpRecyclerView() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = ItemAdapter(this)
     }
 
     private fun loadItems() {
         showLoadingIndicator()
         ItemService(this, this).loadItems()
+        increaseReloadCount()
+    }
+
+    private fun increaseReloadCount() {
+        tracking.reloadCount++
+        Toast.makeText(this, "reload count: ${tracking.reloadCount}", Toast.LENGTH_SHORT).show()
     }
 
     private fun showLoadingIndicator() {
---
+++ app/src/main/kotlin/com/shpock/kotlin/Tracking.kt
@@ -0,0 +1,25 @@
+package com.shpock.kotlin
+
+import android.content.SharedPreferences
+
+
+/**
+ * Created by Daniel Niedermühlbichler on 05/04/16.
+ */
+class Tracking(private val preferences: SharedPreferences) {
+
+    var reloadCount: Int = -1
+        get() {
+            if (field == -1) {
+                field = preferences.getInt(PREF_RELOAD_COUNT, 0)
+            }
+            return field
+        }
+        set(value) {
+            field = value
+            preferences.edit().putInt(PREF_RELOAD_COUNT, value).apply()
+        }
+
+
+    private val PREF_RELOAD_COUNT = "pref_reload_count"
+}
